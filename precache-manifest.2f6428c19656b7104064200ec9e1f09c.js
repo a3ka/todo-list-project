7541fb852fcd4a38fb5eb2eb4e3d7c257458b898
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-04-13 14:37:08 +0300

    Updates

---
+++ precache-manifest.2f6428c19656b7104064200ec9e1f09c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e3b5553588b5d7c77f4e558de99d25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fd203a7b93135b843a1",
+    "url": "/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "145bc30ebe7e6046e0f0",
+    "url": "/static/js/2.cc87b138.chunk.js"
+  },
+  {
+    "revision": "936711c3f4be0c9c88207c43cda44117",
+    "url": "/static/js/2.cc87b138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd203a7b93135b843a1",
+    "url": "/static/js/main.53c543c0.chunk.js"
+  },
+  {
+    "revision": "5a60fc9df39256935c27",
+    "url": "/static/js/runtime-main.1e2b0c03.js"
+  }
+]);
